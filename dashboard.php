2f431dccacadd65fa02022955c2f6cef0291541d
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-06 13:22:10 +0000

    no message

--- dashboard.php
+++ dashboard.php
@@ -961,6 +961,17 @@ desired effect
                                                         <h3 class="box-title">Overview</h3>
                                                     </div><!-- /.box-header -->
                                                     <div class="box-body">
+                                                        <form action="actions.php?a=source_update&source_id=<?php echo $source_id; ?>" method="post" class="form-horizontal">
+                                                            <div class="row">
+                                                                <div class="form-group col-lg-6">
+                                                                    <label for="name" class="col-lg-3 control-label">Name</label>
+                                                                    <div class="col-lg-9">
+                                                                        <input type="text" name="name" id="name" class="form-control" value="<?php echo $source['name']; ?>">
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+
+
                                                         <dl class="dl-horizontal">
                                                             <dt>Name</dt>
                                                             <dd><?php echo $source['name']; ?></dd>
@@ -980,6 +991,7 @@ desired effect
                                                             <dt>Channel</dt>
                                                             <dd><?php echo ucwords(str_replace("_", " ", $source['assigned_channel'])); ?></dd>
                                                         </dl>
+                                                        </form>
                                                     </div>
                                                 </div>
                                             </div>
