b8c3d8e1bc92074b0b38e8cc357e18398ca24f2b
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-07 16:58:47 +0000

    no message

--- xtream_codes_streams.php
+++ xtream_codes_streams.php
@@ -2,6 +2,8 @@
 
 $raw = file_get_contents("http://iptv.genexnetworks.net:10810/player_api.php?username=jamie&password=M!mi1372&action=get_live_streams");
 
+echo $raw;
+
 $json = json_decode($raw, true);
 
 foreach ( $json as $stream ) {
